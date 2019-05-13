d2ada0e72ca27f6aec5d22c2a3e29495118890e3
Alvaro Flaño <tikoflano@gmail.com>
2019-05-13 17:44:00 -0400

    Se instalan las apps definidas en el .env

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -4,17 +4,25 @@ run_occ() {
   su -p www-data -s /bin/sh -c "php /var/www/html/occ $1"
 }
 
+# Maintenance mode on
 run_occ 'maintenance:mode --on'
+
+# Needed config
 run_occ 'db:convert-filecache-bigint -n'
 run_occ 'background:cron'
 run_occ 'config:system:set overwriteprotocol --value="https"'
 run_occ 'config:system:set forwarded_for_headers 0 --value=HTTP_X_FORWARDED_FOR'
 run_occ 'config:system:set trusted_proxies 0 --value="$(hostname -i | cut -d. -f1-3).1"'
-run_occ 'app:install occweb'
-run_occ 'app:install apporder'
-run_occ 'app:install external'
-run_occ 'app:install drawio'
-run_occ 'app:install extract'
-run_occ 'app:install files_accesscontrol'
-run_occ 'app:enable occweb -g admin'
+
+# Apps install
+for APP in ${DEFAULT_APPS//,/ } ${DEFAULT_ADMIN_APPS//,/ };do
+    run_occ "app:install $APP"
+done;
+
+# Apps for admin only
+for APP in ${DEFAULT_ADMIN_APPS//,/ };do
+    run_occ "app:enable $APP -g admin"
+done;
+
+# Maintenance mode off
 run_occ 'maintenance:mode --off'
