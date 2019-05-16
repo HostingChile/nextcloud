b2a85ec6d05df8672bc1e329f0246b07759d5264
Alvaro Flaño <tikoflano@gmail.com>
2019-05-16 13:17:21 -0300

    Mejoras al setup. Ahora es mas simple instalar/actualizar apps y ademas antes de eso se revisa si la API de apps esta disponible, sino se demora mucho para nada

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 run_occ() {
-  su -p www-data -s /bin/sh -c "php /var/www/html/occ $1"
+    su -p www-data -s /bin/sh -c "php /var/www/html/occ $1"
+}
+
+install_update_app() {
+    run_occ "app:install $1" || run_occ "app:update $1"
 }
 
 # Check if Nextcloud is installed
@@ -15,38 +19,6 @@ run_occ 'config:system:set forwarded_for_headers 0 --value=HTTP_X_FORWARDED_FOR'
 run_occ 'config:system:set trusted_proxies 0 --value="$(hostname -i | cut -d. -f1-3).1"'
 run_occ 'config:system:set default_locale --value=es_CL'
 
-# Install selected document editor
-if [ "$DOCUMENT_EDITOR" == "collabora" ];then
-    run_occ "app:remove onlyoffice"
-    run_occ "app:install richdocuments" || run_occ "app:update richdocuments"
-    run_occ "config:app:set richdocuments wopi_url --value=https://$DOCUMENT_EDITOR_HOST"
-elif [ "$DOCUMENT_EDITOR" == "onlyoffice" ];then
-    run_occ "app:remove richdocuments"
-    run_occ "app:install onlyoffice" || run_occ "app:update onlyoffice"
-    run_occ "config:app:set onlyoffice DocumentServerInternalUrl --value=https://$DOCUMENT_EDITOR_HOST/"
-    run_occ "config:app:set onlyoffice DocumentServerUrl --value=https://$DOCUMENT_EDITOR_HOST/"
-    run_occ "config:app:set onlyoffice StorageUrl --value=https://$VIRTUAL_HOST/"
-    run_occ 'config:app:set onlyoffice defFormats --value={\"csv\":\"true\",\"doc\":\"true\",\"docm\":\"true\",\"docx\":\"true\",\"dotx\":\"true\",\"epub\":\"true\",\"html\":\"true\",\"odp\":\"true\",\"ods\":\"true\",\"odt\":\"true\",\"potm\":\"true\",\"potx\":\"true\",\"ppsm\":\"true\",\"ppsx\":\"true\",\"ppt\":\"true\",\"pptm\":\"true\",\"pptx\":\"true\",\"rtf\":\"true\",\"txt\":\"true\",\"xls\":\"true\",\"xlsm\":\"true\",\"xlsx\":\"true\",\"xltm\":\"true\",\"xltx\":\"true\"}'
-    run_occ 'config:app:set onlyoffice editFormats --value={\"csv\":\"true\",\"odp\":\"true\",\"ods\":\"true\",\"odt\":\"true\",\"rtf\":\"true\",\"txt\":\"true\"}'
-fi
-
-# Install and configure antivirus
-run_occ "app:install files_antivirus" || run_occ "app:update files_antivirus"
-run_occ "config:app:set files_antivirus av_host --value=antivirus"
-run_occ "config:app:set files_antivirus av_infected_action --value=delete"
-run_occ "config:app:set files_antivirus av_mode --value=daemon"
-run_occ "config:app:set files_antivirus av_port --value=3310"
-
-# Default apps install and update
-for APP in ${DEFAULT_APPS//,/ } ${DEFAULT_ADMIN_APPS//,/ };do
-    run_occ "app:install $APP" || run_occ "app:update $APP"
-done;
-
-# Apps for admin only
-for APP in ${DEFAULT_ADMIN_APPS//,/ };do
-    run_occ "app:enable $APP -g admin"
-done;
-
 # Set base structure for new users
 BASE_FOLDER="/var/www/html/data/admin/files/Base"
 if [ ! -d "$BASE_FOLDER" ];then
@@ -55,3 +27,38 @@ if [ ! -d "$BASE_FOLDER" ];then
 fi
 run_occ 'files:scan --path="/admin/files"'
 run_occ "config:system:set skeletondirectory --value=$BASE_FOLDER"
+
+# Check if apps API is available
+curl -sf -m10 https://apps.nextcloud.com/api > /dev/null || (echo "Apps API not available" && exit 2)
+
+# Install selected document editor
+if [ "$DOCUMENT_EDITOR" == "collabora" ];then
+    run_occ "app:remove onlyoffice"
+    install_update_app "richdocuments"
+    run_occ "config:app:set richdocuments wopi_url --value=https://$DOCUMENT_EDITOR_HOST"
+elif [ "$DOCUMENT_EDITOR" == "onlyoffice" ];then
+    run_occ "app:remove richdocuments"
+    install_update_app "onlyoffice"
+    run_occ "config:app:set onlyoffice DocumentServerInternalUrl --value=https://$DOCUMENT_EDITOR_HOST/"
+    run_occ "config:app:set onlyoffice DocumentServerUrl --value=https://$DOCUMENT_EDITOR_HOST/"
+    run_occ "config:app:set onlyoffice StorageUrl --value=https://$VIRTUAL_HOST/"
+    run_occ 'config:app:set onlyoffice defFormats --value={\"csv\":\"true\",\"doc\":\"true\",\"docm\":\"true\",\"docx\":\"true\",\"dotx\":\"true\",\"epub\":\"true\",\"html\":\"true\",\"odp\":\"true\",\"ods\":\"true\",\"odt\":\"true\",\"potm\":\"true\",\"potx\":\"true\",\"ppsm\":\"true\",\"ppsx\":\"true\",\"ppt\":\"true\",\"pptm\":\"true\",\"pptx\":\"true\",\"rtf\":\"true\",\"txt\":\"true\",\"xls\":\"true\",\"xlsm\":\"true\",\"xlsx\":\"true\",\"xltm\":\"true\",\"xltx\":\"true\"}'
+    run_occ 'config:app:set onlyoffice editFormats --value={\"csv\":\"true\",\"odp\":\"true\",\"ods\":\"true\",\"odt\":\"true\",\"rtf\":\"true\",\"txt\":\"true\"}'
+fi
+
+# Install and configure antivirus
+install_update_app "files_antivirus"
+run_occ "config:app:set files_antivirus av_host --value=antivirus"
+run_occ "config:app:set files_antivirus av_infected_action --value=delete"
+run_occ "config:app:set files_antivirus av_mode --value=daemon"
+run_occ "config:app:set files_antivirus av_port --value=3310"
+
+# Default apps install and update
+for APP in ${DEFAULT_APPS//,/ } ${DEFAULT_ADMIN_APPS//,/ };do
+    install_update_app "$APP"
+done;
+
+# Apps for admin only
+for APP in ${DEFAULT_ADMIN_APPS//,/ };do
+    run_occ "app:enable $APP -g admin"
+done;
