f25d7dcfa0562f33a436106582c97786bbcf548a
Alvaro Flaño <tikoflano@gmail.com>
2020-04-03 17:00:43 -0300

    Update setup.sh

--- nextcloud/setup.sh
+++ nextcloud/setup.sh
@@ -2,25 +2,24 @@
 set -eu
 
 run_occ() {
-	su -p www-data -s /bin/sh -c "php /var/www/html/occ $1" || true
+  su -p www-data -s /bin/sh -c "php /var/www/html/occ $1" || true
 }
 
 install_update_app() {
-	run_occ "app:install $1" || run_occ "app:update $1"
+  run_occ "app:install $1" || run_occ "app:update $1"
 }
 
-if [[ $SKIP_INITIAL_SETUP != "1" ]] && [[ ! "$(run_occ 'config:system:get inital_setup_completed')" == "yes" ]];then
+if [[ $SKIP_INITIAL_SETUP != "1" ]] && [[ ! "$(run_occ 'config:system:get inital_setup_completed' | tail -n1)" == "yes" ]];then
 	echo "Initial setup started"
 
 	# Check if Nextcloud is installed
-	run_occ 'status' | head -n1 | grep -q true || { echo "Nextcloud is not installed yet"; exit 1; }
+	run_occ 'status' | grep -q "installed: true" || { echo "Nextcloud is not installed yet"; exit 1; }
 
 	# System config
 	run_occ 'db:convert-filecache-bigint -n'
 	run_occ 'background:cron'
 	run_occ 'config:system:set forwarded_for_headers 0 --value=HTTP_X_FORWARDED_FOR'
 	run_occ 'config:system:set default_locale --value=es_CL'
-	run_occ 'db:add-missing-indices'
 
 	# Proxy config
 	run_occ 'config:system:set trusted_proxies 0 --value="$(hostname -i | cut -d. -f1-3).0/24"'
@@ -30,13 +29,13 @@ if [[ $SKIP_INITIAL_SETUP != "1" ]] && [[ ! "$(run_occ 'config:system:get inital
 	run_occ 'maintenance:update:htaccess'
 
 	# Set base structure for new users
-	BASE_FOLDER="/var/www/html/data/${NEXTCLOUD_ADMIN_USER}/files/Base"
+	BASE_FOLDER="/var/www/html/data/admin/files/Base"
 	if [ ! -d "$BASE_FOLDER" ];then
 		mkdir $BASE_FOLDER
 		chown www-data:www-data $BASE_FOLDER
 	fi
 	
-	run_occ 'files:scan --path="/${NEXTCLOUD_ADMIN_USER}/files"'
+	run_occ 'files:scan --path="/admin/files"'
 	run_occ "config:system:set skeletondirectory --value=$BASE_FOLDER"
 
 	# Check if apps API is available
@@ -58,7 +57,8 @@ if [[ $SKIP_INITIAL_SETUP != "1" ]] && [[ ! "$(run_occ 'config:system:get inital
 	run_occ "config:app:set files_antivirus av_port --value=3310"
 
 	# Default apps install and update
-        for APP in ${DEFAULT_APPS//,/ };do
+  DEFAULT_APPS="announcementcenter,apporder,calendar,checksum,contacts,drawio,extract,files_accesscontrol,files_automatedtagging,files_downloadactivity,files_mindmap,files_retention,files_trackdownloads,groupfolders,guests,ldap_write_support,quickaccesssorting,tasks"
+	for APP in ${DEFAULT_APPS//,/ };do
 		install_update_app "$APP"
 	done;
 
@@ -66,6 +66,44 @@ if [[ $SKIP_INITIAL_SETUP != "1" ]] && [[ ! "$(run_occ 'config:system:get inital
 	run_occ "config:app:set files_sharing lookupServerEnabled --value=no"
 	run_occ "config:app:set files_sharing lookupServerUploadEnabled --value=no"
 	run_occ "config:app:set files_sharing outgoing_server2server_share_enabled --value=no"
+		
+	if [ "$LDAP_ENABLED" == "yes" ];then
+		echo "LDAP setup started"
+
+		run_occ "app:enable user_ldap"
+
+		run_occ "ldap:create-empty-config"
+
+		run_occ "ldap:set-config s01 ldapHost ldap"
+		run_occ "ldap:set-config s01 ldapPort 389"
+
+		run_occ "ldap:set-config s01 ldapAgentName cn=admin,${LDAP_BASE_DN}"
+		run_occ "ldap:set-config s01 ldapAgentPassword '${LDAP_PASSWORD}'"
+
+		run_occ "ldap:set-config s01 ldapBase ${LDAP_BASE_DN}"
+		run_occ "ldap:set-config s01 ldapBaseGroups ou=Groups,${LDAP_BASE_DN}"
+		run_occ "ldap:set-config s01 ldapBaseUsers ou=Users,${LDAP_BASE_DN}"
+
+		run_occ "ldap:set-config s01 ldapExpertUsernameAttr cn"
+
+		run_occ "ldap:set-config s01 ldapGroupDisplayName cn"
+		run_occ "ldap:set-config s01 ldapGroupFilter '(&(objectclass=groupOfNames))'"
+		run_occ "ldap:set-config s01 ldapGroupFilterMode 1"
+
+		run_occ "ldap:set-config s01 ldapLoginFilter '(&(cn=%uid))'"
+		run_occ "ldap:set-config s01 ldapLoginFilterMode 1"
+
+		run_occ "ldap:set-config s01 ldapUserDisplayName displayName"
+		run_occ "ldap:set-config s01 ldapUserFilter '(&(objectClass=hSuiteUser))'"
+		run_occ "ldap:set-config s01 ldapUserFilterMode 1"
+
+		run_occ "ldap:set-config s01 turnOnPasswordChange 1"
+		run_occ "ldap:set-config s01 ldapQuotaAttribute quota"
+		
+		run_occ "ldap:set-config s01 ldapConfigurationActive 1"
+
+		echo "LDAP setup finished"
+	fi
 
 	echo "Initial setup finished" && run_occ "config:system:set inital_setup_completed --value=yes"
 else
